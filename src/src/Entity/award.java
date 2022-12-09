c5cf63c9339a787599b06453d55468f666f9e648
Shwnnannw <2737055227@qq.com>
2022-12-09 21:22:56 +0800

    更新了七个成果类

--- src/src/Entity/award.java
+++ src/src/Entity/award.java
@@ -1,4 +1,95 @@
 package Entity;
 
+import java.sql.Blob;
+
 public class award {
+
+    private Master master;
+
+    public award(Master master) {
+        this.master = master;
+    }
+
+    private String id_award;
+    private String name;
+    private int reward_grade;
+    private int award_grade;
+    private String ranking;
+    private String time;
+    private Blob materials;
+
+
+    public award(String id_award, String name, int reward_grade, int award_grade, String ranking, String time, Blob materials) {
+        this.id_award = id_award;
+        this.name = name;
+        this.reward_grade = reward_grade;
+        this.award_grade = award_grade;
+        this.ranking = ranking;
+        this.time = time;
+        this.materials = materials;
+    }
+
+
+    public Master getMaster() {
+        return master;
+    }
+
+    public void setMaster(Master master) {
+        this.master = master;
+    }
+    public String getId_award() {
+        return id_award;
+    }
+
+    public void setId_award(String id_award) {
+        this.id_award = id_award;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReward_grade() {
+        return reward_grade;
+    }
+
+    public void setReward_grade(int reward_grade) {
+        this.reward_grade = reward_grade;
+    }
+
+    public int getAward_grade() {
+        return award_grade;
+    }
+
+    public void setAward_grade(int award_grade) {
+        this.award_grade = award_grade;
+    }
+
+    public String getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(String ranking) {
+        this.ranking = ranking;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Blob getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(Blob materials) {
+        this.materials = materials;
+    }
 }
--- src/src/Entity/patent.java
+++ src/src/Entity/patent.java
@@ -1,4 +1,103 @@
 package Entity;
 
+import java.sql.Blob;
+
 public class patent {
+    Master master;
+
+    public patent(Master master) {
+        this.master = master;
+    }
+
+    private String id_patent;
+    private String name;
+    private int type;
+    private String number;
+    private String time;
+    private String state;
+    private int ranking;
+    private Blob materials;
+
+    public patent(String id_patent, String name, int type, String number, String time, String state, int ranking, Blob materials) {
+        this.id_patent = id_patent;
+        this.name = name;
+        this.type = type;
+        this.number = number;
+        this.time = time;
+        this.state = state;
+        this.ranking = ranking;
+        this.materials = materials;
+    }
+
+    public Master getMaster() {
+        return master;
+    }
+
+    public void setMaster(Master master) {
+        this.master = master;
+    }
+
+    public String getId_patent() {
+        return id_patent;
+    }
+
+    public void setId_patent(String id_patent) {
+        this.id_patent = id_patent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+
+    public Blob getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(Blob materials) {
+        this.materials = materials;
+    }
 }
